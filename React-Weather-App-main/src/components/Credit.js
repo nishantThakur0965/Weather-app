63a90904000f3ccfd392910c69e5a939a006a599
Nishant0965 <nishant0965.be21@chitkara.edu.in>
2024-07-14 17:36:13 +0530

    add files

---
+++ React-Weather-App-main/src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
+        > Nishant Thakur</a
+      >, <a href="https://github.com/nishantThakur0965/Weather-app">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
